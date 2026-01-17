5b30987ea5742909d91c54828aeb04c2c2557b26
Marshlem <sigitas.rancas@gmail.com>
2026-01-17 21:19:11 +0200

    added Dockerfile

--- InvestmentTracker.API/obj/Debug/net8.0/InvestmentTracker.API.AssemblyInfo.cs
+++ InvestmentTracker.API/obj/Debug/net8.0/InvestmentTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestmentTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e0f27b01fcf47d66783c1640770fcd43fbbb2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecaa850438bcc6569941643b2c102736666bd18")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestmentTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestmentTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
